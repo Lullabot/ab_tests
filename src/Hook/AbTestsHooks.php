46e01cf0ae97ab4083c4b291f11e33c15b0a6fa1
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2025-03-27 17:31:31 +0100

    feat: add configuration import/export ignore option

--- src/Hook/AbTestsHooks.php
+++ src/Hook/AbTestsHooks.php
@@ -12,6 +12,7 @@ use Drupal\ab_tests\EntityHelper;
 use Drupal\Component\Plugin\Exception\PluginException;
 use Drupal\Component\Plugin\PluginInspectionInterface;
 use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
 use Drupal\Core\Entity\EntityFormInterface;
@@ -19,6 +20,7 @@ use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Form\SubformState;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Link;
 use Drupal\Core\Plugin\PluginFormInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Routing\RouteObjectInterface;
@@ -54,6 +56,8 @@ class AbTestsHooks {
    *   The route match.
    * @param \Drupal\ab_tests\AbAnalyticsPluginManager $analyticsManager
    *   The analytics manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config factory.
    */
   public function __construct(
     private readonly EntityHelper $entityHelper,
@@ -62,6 +66,7 @@ class AbTestsHooks {
     private readonly AbVariantDeciderPluginManager $variantDeciderManager,
     private readonly RouteMatchInterface $routeMatch,
     private readonly AbAnalyticsPluginManager $analyticsManager,
+    private readonly ConfigFactoryInterface $configFactory,
   ) {}
 
   /**
@@ -171,7 +176,8 @@ class AbTestsHooks {
    */
   #[Hook('hook_preprocess_node')]
   public function preprocessNode(&$variables): void {
-    $route_name = $this->requestStack->getCurrentRequest()->get(RouteObjectInterface::ROUTE_NAME);
+    $route_name = $this->requestStack->getCurrentRequest()
+      ->get(RouteObjectInterface::ROUTE_NAME);
     if ($route_name !== 'ab_tests.render_variant') {
       return;
     }
@@ -210,6 +216,19 @@ class AbTestsHooks {
     if (isset($form['additional_settings']) && $form['additional_settings']['#type'] === 'vertical_tabs') {
       $form['ab_tests']['#group'] = 'additional_settings';
     }
+
+    // Add a message about the export config setting if it's enabled.
+    $config = $this->configFactory->get('ab_tests.settings');
+    $settings_link = Link::createFromRoute($this->t('A/B Tests settings'), 'ab_tests.settings');
+    $message = $config->get('ignore_config_export')
+      ? $this->t('Note: A/B Tests configuration is currently set to be <strong>ignored</strong> during configuration export. This setting can be changed in the @settings_link.', ['@settings_link' => $settings_link->toString()])
+      : $this->t('Note: A/B Tests configuration will be <strong>exported</strong> during configuration export. This setting can be changed in the @settings_link.', ['@settings_link' => $settings_link->toString()]);
+    $form['ab_tests']['export_notice'] = [
+      '#type' => 'markup',
+      '#markup' => '<div class="messages messages--status">' . $message . '</div>',
+      '#weight' => -10,
+    ];
+
     $form['ab_tests']['is_active'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Enable A/B tests'),
